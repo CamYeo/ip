1d40d79a1c82ea68d2d163621819653160ad5653
CamYeo <cameron.yeo@gmail.com>
2026-01-22 19:29:55 +0800

    level 5

--- src/main/java/Lebron.java
+++ src/main/java/Lebron.java
@@ -14,69 +14,73 @@ public class Lebron {
         String input1 = "";
         System.out.println("_".repeat(60));
         while (! input1.equals("bye")) {
-            input1 = sc.nextLine();
-            if (input1.equals("bye")) {
-                break;
-            }
+            try {
+                input1 = sc.nextLine();
+                if (input1.equals("bye")) {
+                    break;
+                } else if (input1.equals("list")) {
+                    for (int i = 0; i < inputlist.size(); i++) {
+                        System.out.println((i + 1) + ". " + inputlist.get(i));
+                    }
 
-            else if (input1.equals("list")) {
-                for (int i =0; i < inputlist.size(); i++) {
-                    System.out.println((i + 1) + ". " + inputlist.get(i));
+                } else if (input1.startsWith("mark")) {
+                    int index = Integer.parseInt(input1.replaceAll(".*?(\\d+)$", "$1")) - 1;
+                    inputlist.get(index).mark();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(inputlist.get(index));
+                } else if (input1.startsWith("unmark")) {
+                    int index = Integer.parseInt(input1.replaceAll(".*?(\\d+)$", "$1")) - 1;
+                    inputlist.get(index).unmark();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println(inputlist.get(index));
+                } else if (input1.startsWith("todo")) {
+                    if (input1.length() == 4) {
+                        throw new LebronException("Missing Description");
+                    }
+                    String name = input1.substring(5).trim();
+                    if (name.isEmpty()) {
+                        throw new LebronException("Missing Description");
+                    }
+                    inputlist.add(new Todo(name));
+
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(inputlist.get(inputlist.size() - 1));
+                    System.out.println("Now you have " + inputlist.size() + " tasks in the list.");
+                } else if (input1.startsWith("deadline")) {
+                    String[] parts = input1.substring(9).split("/by");
+                    String name = parts[0].trim();
+                    String by = parts[1].trim();
+
+                    inputlist.add(new Deadline(name, by));
+
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(inputlist.get(inputlist.size() - 1));
+                    System.out.println("Now you have " + inputlist.size() + " tasks in the list.");
+                } else if (input1.startsWith("event")) {
+                    String withoutCommand = input1.substring(6);
+                    String[] partsFrom = withoutCommand.split("/from");
+                    String name = partsFrom[0].trim();
+
+                    String[] partsTo = partsFrom[1].split("/to");
+                    String from = partsTo[0].trim();
+                    String to = partsTo[1].trim();
+
+                    inputlist.add(new Event(name, from, to));
+
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(inputlist.get(inputlist.size() - 1));
+                    System.out.println("Now you have " + inputlist.size() + " tasks in the list.");
+
+                } else {
+                    throw new LebronException("No valid task was created");
                 }
-
+                System.out.println("_".repeat(60));
             }
-            else if (input1.startsWith("mark")) {
-                int index = Integer.parseInt(input1.replaceAll(".*?(\\d+)$", "$1")) - 1;
-                inputlist.get(index).mark();
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(inputlist.get(index));
+            catch (LebronException e) {
+                System.out.println("_".repeat(60));
+                System.out.println(e.getMessage());
+                System.out.println("_".repeat(60));
             }
-            else if (input1.startsWith("unmark")) {
-                int index = Integer.parseInt(input1.replaceAll(".*?(\\d+)$", "$1")) - 1;
-                inputlist.get(index).unmark();
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println(inputlist.get(index));
-            }
-            else if (input1.startsWith("todo")) {
-                String name = input1.substring(5).trim();
-                inputlist.add(new Todo(name));
-
-                System.out.println("Got it. I've added this task:");
-                System.out.println(inputlist.get(inputlist.size() - 1));
-                System.out.println("Now you have " + inputlist.size() + " tasks in the list.");
-            }
-            else if (input1.startsWith("deadline")) {
-                String[] parts = input1.substring(9).split("/by");
-                String name = parts[0].trim();
-                String by = parts[1].trim();
-
-                inputlist.add(new Deadline(name, by));
-
-                System.out.println("Got it. I've added this task:");
-                System.out.println(inputlist.get(inputlist.size() - 1));
-                System.out.println("Now you have " + inputlist.size() + " tasks in the list.");
-            }
-            else if (input1.startsWith("event")) {
-                String withoutCommand = input1.substring(6);
-                String[] partsFrom = withoutCommand.split("/from");
-                String name = partsFrom[0].trim();
-
-                String[] partsTo = partsFrom[1].split("/to");
-                String from = partsTo[0].trim();
-                String to = partsTo[1].trim();
-
-                inputlist.add(new Event(name, from, to));
-
-                System.out.println("Got it. I've added this task:");
-                System.out.println(inputlist.get(inputlist.size() - 1));
-                System.out.println("Now you have " + inputlist.size() + " tasks in the list.");
-
-            }
-            else {
-                inputlist.add(new Task(input1));
-                System.out.println("added: " + input1);
-            }
-            System.out.println("_".repeat(60));
 
         }
         System.out.println("_".repeat(60));
