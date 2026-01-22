c2aafcc41149b7de5c3259e01c7375b81fd0b483
CamYeo <cameron.yeo@gmail.com>
2026-01-22 17:01:37 +0800

    Level 0 commit

---
+++ src/main/java/Lebron.java
@@ -0,0 +1,16 @@
+public class Lebron {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("_".repeat(60));
+        System.out.println("Hello from Lebron!");
+        System.out.println("What can I do for you?");
+        System.out.println("_".repeat(60));
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_".repeat(60));
+
+    }
+}
