e7bdd9ca44a7c16e7da346da1b5ebd01db6a2df0
CamYeo <cameron.yeo@gmail.com>
2026-01-22 18:17:03 +0800

    Level 3

--- src/main/java/Lebron.java
+++ src/main/java/Lebron.java
@@ -5,12 +5,7 @@ import java.util.ArrayList;
 
 public class Lebron {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        List<String> inputlist = new ArrayList<>();
+        List<task> inputlist = new ArrayList<>();
         System.out.println("_".repeat(60));
         System.out.println("Hello from Lebron!");
         System.out.println("What can I do for you?");
@@ -26,8 +21,20 @@ public class Lebron {
                 }
 
             }
+            else if (input1.startsWith("mark")) {
+                int index = Integer.parseInt(input1.replaceAll(".*?(\\d+)$", "$1")) - 1;
+                inputlist.get(index).mark();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(inputlist.get(index));
+            }
+            else if (input1.startsWith("unmark")) {
+                int index = Integer.parseInt(input1.replaceAll(".*?(\\d+)$", "$1")) - 1;
+                inputlist.get(index).unmark();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(inputlist.get(index));
+            }
             else {
-                inputlist.add(input1);
+                inputlist.add(new task(input1, 0));
                 System.out.println("added: " + input1);
             }
             System.out.println("_".repeat(60));
@@ -38,5 +45,34 @@ public class Lebron {
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("_".repeat(60));
 
+    }
+}
+
+class task {
+    String name;
+    int status;
+
+    public task(String name, int status) {
+        this.name = name;
+        this.status = status;
+    }
+
+    public void mark() {
+        this.status = 1;
+    }
+
+    public void unmark() {
+        this.status = 0;
+    }
+
+
+    @Override
+    public String toString() {
+       if (status == 1) {
+           return "[X] " + this.name;
+       }
+       else {
+           return "[ ] " + this.name;
+       }
     }
 }
