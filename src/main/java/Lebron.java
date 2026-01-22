11567d144c6d90b31e2d205747b4ca3dc2f63f65
CamYeo <cameron.yeo@gmail.com>
2026-01-22 17:15:32 +0800

    Level 1 Commit

--- src/main/java/Lebron.java
+++ src/main/java/Lebron.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Lebron {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -8,6 +10,17 @@ public class Lebron {
         System.out.println("_".repeat(60));
         System.out.println("Hello from Lebron!");
         System.out.println("What can I do for you?");
+        System.out.println("_".repeat(60));
+        Scanner sc = new Scanner(System.in);
+        String input1 = sc.next();
+        System.out.println("_".repeat(60));
+        while (! input1.equals("bye")) {
+            System.out.println(input1);
+            System.out.println("_".repeat(60));
+            input1 = sc.next();
+            System.out.println("_".repeat(60));
+        }
+        System.out.println(input1);
         System.out.println("_".repeat(60));
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("_".repeat(60));
