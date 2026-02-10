7521251769b1334a933cc4a777bd768ddb7d0f47
CamYeo <cameron.yeo@gmail.com>
2026-02-10 20:03:38 +0800

    Add help command for in-app user guidance
    Implement a help command that displays a formatted list of available
    commands and usage examples. Add help message generation in Ui and
    integrate it into getResponse for GUI command handling.

--- src/main/java/lebron/Ui.java
+++ src/main/java/lebron/Ui.java
@@ -38,5 +38,43 @@ public class Ui {
             System.out.println((i + 1) + ". " + tasks.get(i));
         }
     }
+
+    public String getHelpMessage() {
+        return """
+Hello! Here are the commands you can use:
+
+list
+  Show all tasks
+
+todo <description>
+  Add a todo task
+  Example: todo read book
+
+deadline <description> /by <date>
+  Add a deadline task
+  Example: deadline submit report /by Friday
+
+event <description> /from <start> /to <end>
+  Add an event task
+  Example: event meeting /from 2pm /to 4pm
+
+mark <task number>
+unmark <task number>
+  Mark or unmark a task
+
+delete <task number>
+  Delete a task
+
+find <keyword>
+  Search for tasks containing a keyword
+
+help
+  Show this help message
+
+bye
+  Exit the application
+""";
+    }
+
 }
 
